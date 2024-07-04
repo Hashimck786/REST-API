7a6c124b4bcf23d786f85ff7b0dfdb20fe851aa6
Hashimck786 <hsmhashim786@gmail.com>
2024-07-04 14:09:00 +0530

    delete user with id added

--- router/users.js
+++ router/users.js
@@ -2,7 +2,7 @@ import express, { Router } from 'express'
 import { v4 as uuidv4 } from 'uuid';
 const router = express.Router();
 
-const users = [
+let users = [
     
 ];
 
@@ -28,6 +28,13 @@ router.get('/:id', (req,res) => {
   const {id} = req.params ;
   const foundUser = users.find((user) => user.id === id)
   res.send(foundUser)
+})
+
+
+router.delete('/:id',(req,res) => {
+  const {id} = req.params ;
+  users = users.filter((user)=>user.id !== id)
+  res.send('deleted user')
 })
 
 
